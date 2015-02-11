9f13af414f18771c23f5b60dc9a5baa5ff246564
Rolf Bachmann <rbachmann@me.com>
2015-02-11 21:02:07 +0100

    Upload 4D V14 version

---
+++ jsonEditor_V14/Resources/jsonEditor/bootstrap.js
@@ -0,0 +1,25 @@
+/**
+ * load the chrome application on body load
+ */
+window.onload = function () {
+  app.load();
+  app.resize();
+  document.getElementById('contents').style.visibility = 'visible';
+	document.getElementById('footer').style.visibility = 'hidden';
+  	document.getElementById('menu').style.visibility = 'hidden';
+};
+
+// override ajax, we may need to ask additional permissions from the user
+var _fetch = ajax.fetch;
+ajax.fetch = function (method, url, body, headers, callback) {
+  chrome.permissions.request({
+    origins: [url]
+  }, function (granted) {
+    if (granted) {
+      _fetch(method, url, body, headers, callback);
+    }
+    else {
+      callback(new Error('No permission granted'), 0);
+    }
+  });
+};
